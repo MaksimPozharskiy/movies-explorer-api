2cc21b0123522af28a8f018df5ac3b949df9ac3c
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-02-15 11:11:24 +0300

    feat: добавить CORS мидлвару для междоменных запросов

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ mongoose.connect(DB_ADRESS, {
   useUnifiedTopology: true,
 });
 
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  }
+  next();
+});
+
+// Мидлвары
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
